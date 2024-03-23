85aa734f2d6aac04a0b8068cf06f520c89ff69dd
atomtables <76572470+atomtables@users.noreply.github.com>
2024-03-23 11:55:40 -0400

    adding schemas, redid database, added models for blog posts, added views to api to send to frontend

--- blogsite/admin.py
+++ blogsite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from blogsite.models import Comment, BlogPost, BlogImage
+
 # Register your models here.
+admin.site.register(Comment)
+admin.site.register(BlogPost)
+admin.site.register(BlogImage)
--- blogsite/views.py
+++ blogsite/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from ninja import Router
 
-# Create your views here.
+from atomtables_website.schemas import SuccessData
+from blogsite.models import BlogPost
+
+api = Router()
+
+
+@api.get("/list")
+def blog_list(request):
+    return {
+    }
+
+
+@api.get("/post/{post_id}")
+def blog_post(request, post_id: str):
+    blog = BlogPost.objects.get(id=post_id)
+    return SuccessData(info={
+        "author": {
+            "username": blog.author.username,
+            "first_name": blog.author.first_name,
+            "last_name": blog.author.last_name,
+            "pfp": blog.author.profile.profile_picture.url
+        },
+        "title": blog.title,
+        "subtitle": blog.subtitle,
+        "content": blog.content,
+        "titleImage": blog.title_image.url,
+        "titleCaption": blog.title_image_caption,
+        "blogImages": {
+            "count": blog.blog_images.count(),
+            "images": [image.image.url for image in blog.blog_images.order_by('blog_image_id')],
+            "captions": [image.caption for image in blog.blog_images.order_by('blog_image_id')]
+        },
+        "comments": {
+            "count": blog.comments.count(),
+            "comments": [{
+                "user": {
+                    "username": comment.user.username,
+                    "first_name": comment.user.first_name,
+                    "last_name": comment.user.last_name,
+                    "pfp": comment.user.profile.profile_picture.url
+                },
+                "content": comment.blog_content
+            } for comment in blog.comments.all()]
+        }
+    })
